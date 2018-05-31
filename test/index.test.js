a31deaed3d07ef80dcc7818590e6d27335450204
DJL箫氏 <1281233206@qq.com>
2018-05-31 17:07:19 +0800

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+/**
+ * @Author JohnLi
+ * @Date 2018/5/23 20:30
+ */
+import { typeCheck, isString } from '../src/index'
+describe('index.js的测试', function () {
+  before (function () {
+    console.log('start test')
+  })
+  it('1应该是数字', function() {
+    typeCheck(1).should.equal('number')
+  })
+  it('"1" 应该是字符', function() {
+    isString('1').should.equal(true)
+  })
+  after (function () {
+    console.log('end test')
+  })
+})
